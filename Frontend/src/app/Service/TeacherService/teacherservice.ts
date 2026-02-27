bfdb09ee4f37d69d1566d733363966e28abf0b7f
Sujalgopani <sujalgopani34@gmail.com>
2026-02-27 18:29:35 +0530

    Complete Crud on teacher side

--- Frontend/src/app/Service/TeacherService/teacherservice.ts
+++ Frontend/src/app/Service/TeacherService/teacherservice.ts
@@ -5,9 +5,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Teacherservice {
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  GetAllTeacher(){
-    return this.http.get<any>("https://localhost:44385/api/Admin/GetAllTeacher");
+  GetAllTeacher() {
+    return this.http.get<any>('https://localhost:44385/api/Admin/GetAllTeacher');
+  }
+
+  UpdateTeacher(Userid: number, Data: any) {
+    return this.http.put<any>(`https://localhost:44385/api/Admin/UpdateTeacher/${Userid}`, Data);
+  }
+ 
+  Deleteteacher(Userid: number) {
+    return this.http.delete(`https://localhost:44385/api/Admin/DeleteTeacher/${Userid}`, {
+      responseType: 'text',
+    });
   }
 }
--- Frontend/src/app/teachermain/teachermain.ts
+++ Frontend/src/app/teachermain/teachermain.ts
@@ -1,28 +1,226 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Teacherservice } from '../Service/TeacherService/teacherservice';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginRegisterService } from '../Service/Login-Register/login-register-service';
+import { userInfo } from 'os';
 
+declare var bootstrap: any;
 @Component({
   selector: 'app-teachermain',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './teachermain.html',
   styleUrl: './teachermain.css',
 })
 export class Teachermain implements OnInit {
-  constructor(private teacher_service: Teacherservice) {}
-  users = [];
+  registerForm!: FormGroup;
+  editForm!: FormGroup;
+  editModalInstance: any;
+  deleteModalInstance: any;
+
+  constructor(
+    private teacher_service: Teacherservice,
+    private cdr: ChangeDetectorRef,
+    private fb: FormBuilder,
+  ) {}
+  users: any[] = [];
+  Isloading = false;
+  IsSubmitting = false;
+
+  @ViewChild('exampleModal') exampleModal!: ElementRef;
+  @ViewChild('Editmodel') Editmodel!: ElementRef;
+  @ViewChild('DeleteModal') DeleteModal!: ElementRef;
 
   ngOnInit(): void {
-    this.GetTeacher(); // 🔥 auto call
+    this.GetTeacher();
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      middleName: ['', [Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      password: ['', Validators.required],
+      conformpassword: ['', Validators.required],
+      role: ['', Validators.required],
+      isActive: [true],
+    });
+
+    this.editForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      middleName: [''],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+      newpassword: [''],
+      isActive: [true],
+    });
   }
+  service = inject(LoginRegisterService);
 
   GetTeacher() {
+    this.Isloading = true;
     this.teacher_service.GetAllTeacher().subscribe({
       next: (res) => {
-        console.log(res);
-        this.users = res; // 🔥 IMPORTANT
+        this.users = res;
+        this.Isloading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.Isloading = false;
+        console.log(err);
+      },
+    });
+  }
+
+  showAddForm = false;
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key.toLowerCase() === 'e') {
+      event.preventDefault(); // 🔥 IMPORTANT (default select all stop)
+
+      this.openAddForm();
+    }
+  }
+
+  openAddForm() {
+    const modal = new bootstrap.Modal(this.exampleModal.nativeElement);
+    modal.show();
+  }
+
+  closeAddForm() {
+    const modal = bootstrap.Modal.getInstance(this.exampleModal.nativeElement);
+    modal?.hide();
+  }
+
+  SendOtp = false;
+  tempemail = '';
+  IsAdd = false;
+  AddUser() {
+    const formData = { ...this.registerForm.value };
+    delete formData.conformpassword;
+    console.log(formData);
+    this.IsAdd = true;
+    this.SendOtp = true;
+    this.service.RegisterUser(formData).subscribe({
+      next: (res) => {
+        this.IsAdd = false;
+        this.tempemail = formData.email;
+        this.SendOtp = true;
+        console.log('Done');
+        this.GetTeacher();
+      },
+      error: (err) => {
+        this.IsAdd = false;
+        console.log('Err');
+      },
+    });
+  }
+
+  otpValue = '';
+  VarifyOtp() {
+    const OtpObj = {
+      email: this.registerForm.get('email')?.value,
+      otp: this.otpValue,
+    };
+    console.log(OtpObj);
+    this.service.OtpVarify(OtpObj).subscribe({
+      next: (res) => {
+        console.log('Done');
+        this.SendOtp = false;
+        this.otpValue = '';
+        this.closeAddForm();
+        this.registerForm.reset();
+      },
+      error: (err) => {
+        console.log('Err');
+      },
+    });
+  }
+
+  // edit
+
+  openEditForm() {
+    const modal = new bootstrap.Modal(this.Editmodel.nativeElement);
+    modal.show();
+  }
+
+  closeEditForm() {
+    const modal = bootstrap.Modal.getInstance(this.Editmodel.nativeElement);
+    modal?.hide();
+  }
+  editId: number | null = null;
+  EditUser(user: any) {
+    this.editId = user.userId;
+
+    this.editForm.patchValue({
+      firstName: user.firstName,
+      middleName: user.middleName,
+      lastName: user.lastName,
+      email: user.email,
+      phone: user.phone,
+      newpassword: '', // empty
+      isActive: user.isActive,
+    });
+
+    this.openEditForm();
+  }
+
+  UpdateUser() {
+    if (this.editForm.invalid) return;
+
+    const formData = { ...this.editForm.value };
+
+    formData.isActive = formData.isActive ? true : false;
+
+    this.teacher_service.UpdateTeacher(this.editId!, formData).subscribe({
+      next: (res) => {
+        console.log('Updated Successfully');
+        this.closeEditForm();
+        this.GetTeacher();
       },
       error: (err) => {
         console.log(err);
+      },
+    });
+    console.log(this.editForm.value);
+  }
+
+
+
+  // Delete Model
+  DeleteId: number | null = null;
+  deleteteacher(Userid: number) {
+    console.log(Userid);
+    this.DeleteId = Userid;
+    this.deleteModalInstance = new bootstrap.Modal(this.DeleteModal.nativeElement);
+    this.deleteModalInstance.show();
+  }
+
+  closeDeleteModal() {
+    this.deleteModalInstance?.hide();
+    this.DeleteId = null; // reset
+  }
+
+  ConfirmDelete() {
+    if (!this.DeleteId) return;
+
+    this.teacher_service.Deleteteacher(this.DeleteId).subscribe({
+      next: () => {
+        this.closeDeleteModal();
+        this.GetTeacher();
       },
     });
   }
